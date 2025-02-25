c2bcac743861156f898f01de4a3ce38600b29c29
HaierKeys <haierkeys@gmail.com>
2025-02-25 18:25:26 +0800

    init

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Sing-box Node Config Server Tools /
+
+----
+从节点文件读取 `节点信息` 并生成完整的 `sing-box 配置信息`
+
+## 使用说明
+### 安装模块
+
+```bash
+pip install jinja2
+```
+
+### 运行脚本
+```
+python3 sev.py
+```
+
+### 使用说明
+默认 8000 端口, 访问地址为 `http://127.0.0.1:8000/?type=ios&password=password`
+
+请求参数说明
+| 参数     | 说明                           | 其他       |
+|----------|------------------------------|------------|
+| type     | 类型,用于多端,返回不同配置使用 | 例如 linux |
+| password | 访问密码                       |            |
+
+
+### 规则模版说明
+脚本默认从node.json 读取所有节点信息
+
+
+{{ allNodes }} 生成所有节点详细配置列表, 不含 json 的 []
+{{ NodeNameFilter() }} 生成所有节点名字列表, 含有[]
+{{ NodeNameFilter('日本') }} 生成 tag 为 日本的节点名字列表, 含有[]
+
+{% if setType == 'linux' %} "auto_redirect": true,{% else %} "auto_route": true, {% endif %} 用于多端返回不同配置
---
+++ sev.py
@@ -0,0 +1,75 @@
+import http.server
+import json
+import os
+from jinja2 import Environment, FileSystemLoader
+import urllib.parse
+
+envPassword = "password"
+envServerPort = 8000
+nodeName = []
+def NodeNameFilter(name: str = ""):
+    list = []
+    for item in nodeName:
+        if name == "":
+            list.append(item)
+        elif name in item:
+            list.append(item)
+    # 自定义函数逻辑
+    return json.dumps(list, ensure_ascii=False)
+
+class JSONRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # 设置响应状态码和头部信息
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+
+
+        parsed_url = urllib.parse.urlparse(self.path)
+        query_params = urllib.parse.parse_qs(parsed_url.query)
+        setType = query_params.get('type', [''])[0]
+        password = query_params.get('password', [''])[0]
+
+        if password != envPassword:
+            self.wfile.write("Password Error".encode('utf-8'))
+            return
+
+
+
+        # 读取节点信息
+        node_file = 'node.json'
+        if os.path.exists(node_file):
+            # 读取 JSON 文件内容
+            with open(node_file, 'r', encoding='utf-8') as file:
+                node_data = json.load(file)
+
+        # 设置全部节点变量
+        allNodes = json.dumps(node_data, ensure_ascii=False)[1:-1]
+
+        # 读取规则模版
+        template_file = 'template.json'
+        # 获取当前脚本所在的目录路径
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # 创建 Jinja2 环境，指定模板加载器
+        env = Environment(loader=FileSystemLoader(current_dir))
+        env.globals['NodeNameFilter'] = NodeNameFilter
+
+        # 加载模板文件
+        template = env.get_template(template_file)
+
+        json_content = template.render(
+            allNodes=allNodes,
+            setType=setType
+        )
+        self.wfile.write(json_content.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    # 设置服务器地址和端口
+    server_address = ('', envServerPort)  # 监听所有可用的接口，端口号为 envServerPort
+    # 创建 HTTP 服务器实例
+    httpd = http.server.HTTPServer(server_address, JSONRequestHandler)
+    print('Server is running on port '+ str(envServerPort)+'...')
+    # 启动服务器，开始监听请求
+    httpd.serve_forever()
